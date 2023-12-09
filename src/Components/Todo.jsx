382e6e47820755a06ac2c0f187132284d58c88b3
Daksh Singh Rathore <dakshrathore1729@gmail.com>
2023-12-09 11:02:39 +0530

    style: CSS completed

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import "./CSS/Todo.css"
+import { useRef } from "react";
+import TodoItems from "./TodoItems";
+
+
+let count =0;
+const Todo = () => {
+    const [todos, setTodos] = useState([]);
+    const inputRef = useRef(null);
+    const add = () => { 
+        setTodos([...todos,{no:count++, text:inputRef.current.value, display:""}]) ;
+        inputRef.current.value = "";
+        localStorage.setItem("todos_count",count);
+    }
+
+        useEffect( () => {
+            setTodos(JSON.parse(localStorage.getItem("todos")))
+            count = localStorage.getItem("todos_count");
+        },[])
+    // Now we will create a useEffect hook to store our todos in local storage
+        useEffect(() => {
+            
+            setTimeout(() =>{
+                console.log(todos);
+            // Now we will create local storage for storing our todos, we need to convert our todos array into string
+            localStorage.setItem("todos",JSON.stringify(todos));
+            })
+        },[todos])
+
+  return (
+    <div className="todo">
+        <div className="todo-header">To-Do List</div>
+        <div className="todo-add">
+            <input ref={inputRef} type="text" placeholder="add your task" className="todo-input" />
+            <div onClick={() =>{add()}} className="todo-add-btn">ADD</div>
+        </div>
+        <div className="todo-list">
+        {todos.map((item,index) => {
+            return <TodoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text}/>
+        })}
+        </div>
+      </div>
+  )
+}
+
+export default Todo
---
+++ src/Components/TodoItems.jsx
@@ -0,0 +1,31 @@
+import "./CSS/TodoItems.css"
+
+const TodoItems = ({no,display,text, setTodos}) => {
+
+  const toggle = (no) =>{
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for(let i=0;i<data.length;i++){
+        if(data[i].no===no){
+            data[i].display = data[i].display===""?"line-through":"";
+        }
+    }
+    setTodos(data)
+  }
+
+  return (
+    <div className="todoitems">
+        <div className={'todoitems-container ${display}' } onClick={()=>{toggle(no)}}>
+      {display===""?  <img src="https://github.com/dakshsinghrathore/Brighter-Beginnings/assets/115932772/ea92820d-b933-4864-a97d-39dad63c0b33" alt="" />: <img src="https://github.com/dakshsinghrathore/Brighter-Beginnings/assets/115932772/3fade258-f422-4a3e-9eb7-4939ff3e925b" alt="" />} 
+        
+        <div className="todoitems-text">
+            {text}
+
+        </div>
+
+        </div>
+        <img className="todoitems-cross-icon " src="https://github.com/dakshsinghrathore/Brighter-Beginnings/assets/115932772/724195af-6917-4f6a-bb4d-6650e5b27960" alt="" />
+    </div>
+  )
+}
+
+export default TodoItems
